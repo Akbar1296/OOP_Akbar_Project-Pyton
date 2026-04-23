3d2ddb379ef834cf180f7ef9e0bb0943ba28733c
Akbar farel aminudin <akbarkece0117@gmail.com>
2026-04-23 09:01:44 +0700

    Add files via upload

---
+++ Tugas4_OOP.py
@@ -0,0 +1,15 @@
+
+nama = input("Masukkan nama:") 
+print(nama) 
+
+umur = input("Masukkan umur:")
+print("umur saya",umur)
+
+no_hp = input("Nomor Saya HP:")
+print(no_hp) 
+
+email = input("email saya:")
+print(email)
+
+alamat = input ("Alamat saya:")
+print(alamat)
